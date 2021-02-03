4527ced69cf3a4905ce6be2b29b9f05aa3ef12d5
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-03 12:46:37 +0900

    Brew formula update for go-book-teacher version v1.0.0

--- Formula/go-book-teacher.rb
+++ Formula/go-book-teacher.rb
@@ -10,15 +10,15 @@ class GoBookTeacher < Formula
 
   if OS.mac?
     url "https://github.com/hiromaily/go-book-teacher/releases/download/v1.0.0/go-book-teacher_1.0.0_Darwin_x86_64.tar.gz"
-    sha256 "586e7383aff7c432e3fca3c78a8abd276cabe30ca0a322a8b767d110f6edaa6b"
+    sha256 "d6ba64e321f1c0d946cc8e03bde7ecc2f26cb18d9666c94466140fef0916f72e"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/hiromaily/go-book-teacher/releases/download/v1.0.0/go-book-teacher_1.0.0_Linux_x86_64.tar.gz"
-    sha256 "798e776ce8fd75c0d894b06dd3297551482ba0a6a827f5b511fe87e1f60fcea1"
+    sha256 "29cfbbe64940b5682bdf542f89eda9e66b3cf41626bf41407b317beb3704bb8d"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/hiromaily/go-book-teacher/releases/download/v1.0.0/go-book-teacher_1.0.0_Linux_arm64.tar.gz"
-    sha256 "fd2622c273a83713cde048f640d4686b1a27cc5cafc20caa5fff99e5cbe2a9bb"
+    sha256 "4d17e872b3649bb666e3a93a811cebf2461ad63e46b980b8b37361ab3da17ae7"
   end
 
   def install
