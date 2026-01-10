94b433b0a041fcce811be30ab5318cb312a6d304
HirokiYasui <hiromaily2@gmail.com>
2026-01-10 12:05:46 +0900

    Update yaml-lint checksums for v0.1.0

--- Formula/yaml-lint.rb
+++ Formula/yaml-lint.rb
@@ -11,24 +11,24 @@ class YamlLint < Formula
   on_macos do
     on_intel do
       url "https://github.com/hiromaily/yaml-lint-rs/releases/download/v#{version}/yaml-lint-x86_64-apple-darwin.tar.gz"
-      sha256 "b62f9bcbcb45271846bb4f0fcb563936ffe129aa7908b9f630b00fe4fe6ff630"
+      sha256 "3e7cae5d0092d103c4bf2e29c1cb7fbaf8b272409780c97793840d6e9d8b0292"
     end
 
     on_arm do
       url "https://github.com/hiromaily/yaml-lint-rs/releases/download/v#{version}/yaml-lint-aarch64-apple-darwin.tar.gz"
-      sha256 "1da584980274ca4167b33b5aa75ef54917356b87a7300f7e517332557dec7e6d"
+      sha256 "ead4c6abae47cc02a6d615a15781b8d68a27aaf73ced8b4384c3a279b3bae470"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/hiromaily/yaml-lint-rs/releases/download/v#{version}/yaml-lint-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "e0a7945af1562a94d9fb8305a5107ac4e5101194acb2f7fcaca69514ff9afdd7"
+      sha256 "be6068ee4bdcb80f2cd4e1e1bbe23905e2162810e4a280b3fc2e9ee6126ae603"
     end
 
     on_arm do
       url "https://github.com/hiromaily/yaml-lint-rs/releases/download/v#{version}/yaml-lint-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "000d182c3545adff2e62916059020ae03952f37e1e6e2a766cf70401646fdce2"
+      sha256 "45e3bf29b6855b20c45b0b6de4613616b08e0738d20c7474345bf53bec430e3f"
     end
   end
 
